ebc205b4857e7e7a7a25923b0897722ad9eb6938
JacyAnderson <jacyjamesanderson@gmail.com>
2017-04-10 15:32:54 -0600

    add heroku support????

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/personal-api");
+                  "https://tranquil-beach-76796.herokuapp.com/");
 
 // module.exports.Campsite = require("./campsite.js.example");
